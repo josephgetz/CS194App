d6537f1debad88f6e3e6a1ab2883aff4e7cc26de
josephgetz <jogetz@stanford.edu>
2016-02-04 17:59:17 -0800

    Updated the ish

--- parseData.py
+++ parseData.py
@@ -1,5 +1,5 @@
 # runs with the command:
-# python parsedata.py spreadsheet.csv Roth Fall 8
+# python parsedata.py WinWk1Grove.csv Grove Winter 1
 
 import json,httplib
 
@@ -17,27 +17,33 @@ def parseFile():
 			line=line.strip().split(',')
 			if '/' in line[0]:
 				spreadsheet.append(line[:7])
-				print line[:7]
-
+				# print line[:7]
 			# else:
 			# 	print line
-
+# Date,Payee (Individual/Business name),Item - Description,Check # ,credit/income,Open/Closed Kitchen Expense,Social Expense
 
 
 def publish():
 	connection = httplib.HTTPSConnection('api.parse.com', 443)
 	connection.connect()
-	connection.request('POST', '/1/classes/GameScore', json.dumps({
-	       "HouseName": HouseName,
-	       "quarter": quarter,
-	       "Week": week,
-	       "Data": spreadsheet
-	     }), {
-	       "X-Parse-Application-Id": "sBDcY6elVaGx4EHc9HYPNnCry7XOMdjuntIDztLD",
-	       "X-Parse-REST-API-Key": "F5KV5T6tI1hKJZS0puRsS3FB32HUqlhRSwMdZmRm",
-	       "Content-Type": "application/json"
-	     })
-	results = json.loads(connection.getresponse().read())
+	for line in spreadsheet:
+		connection.request('POST', '/1/classes/Transactions', json.dumps({
+		       "HouseName": HouseName,
+		       "Quarter": quarter,
+		       "Week": week,
+		       "TransactionDate": line[0],
+		       "Payee":line[1],
+		       "ItemOrDescription": line[2],
+		       # "CheckNum": line[3],
+		       "CreditOrIncome": float(line[4][1:]),
+		       "KitchenExpense": float(line[5][1:]),
+		       "SocialExpense": float(line[6][1:])
+		     }), {
+		       "X-Parse-Application-Id": "sBDcY6elVaGx4EHc9HYPNnCry7XOMdjuntIDztLD",
+		       "X-Parse-REST-API-Key": "F5KV5T6tI1hKJZS0puRsS3FB32HUqlhRSwMdZmRm",
+		       "Content-Type": "application/json"
+		     })
+		results = json.loads(connection.getresponse().read())
 	print results
 
 
